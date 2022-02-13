f1d29e459754e8fcf64087f485efef48994a12e9
ArchieMeng <archiemeng@protonmail.com>
2022-02-13 21:45:26 +0800

    Allow setup.py to read cmake flags from CMAKE_FLAGS environment variable and enable macOS CI
    Signed-off-by: ArchieMeng <archiemeng@protonmail.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import cmake_build_extension
+import os
 import pathlib
 import setuptools
+
+import cmake_build_extension
+
+cmake_flags = [
+    "-DBUILD_SHARED_LIBS:BOOL=OFF",
+    "-DCALL_FROM_SETUP_PY:BOOL=ON",
+]
+if "CMAKE_FLAGS" in os.environ:
+    flags = os.environ["CMAKE_FLAGS"]
+    cmake_flags.extend(flags.split())
 
 setuptools.setup(
     ext_modules=[
@@ -11,10 +21,7 @@ setuptools.setup(
             install_prefix="realsr_ncnn_vulkan_python",
             write_top_level_init="from .realsr_ncnn_vulkan import Realsr",
             source_dir=str(pathlib.Path(__file__).parent / "realsr_ncnn_vulkan_python"),
-            cmake_configure_options=[
-                "-DBUILD_SHARED_LIBS:BOOL=OFF",
-                "-DCALL_FROM_SETUP_PY:BOOL=ON",
-            ],
+            cmake_configure_options=cmake_flags,
         )
     ],
     cmdclass={"build_ext": cmake_build_extension.BuildExtension},
